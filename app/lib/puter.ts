cf3b3ed6fd7910f257d19210f9285bcc18ace6cd
Ray-Shubham <raysubham555@gmail.com>
2025-07-25 17:08:39 +0530

    Cloud Storage, PDF-Image conversion, Key-Value storage and AI Feedback

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
           ],
         },
       ],
-      { model: "claude-sonnet-4" }
+      { model: "claude-3-7-sonnet" }, // { model: "claude-instant-1" }
     ) as Promise<AIResponse | undefined>;
   };
 
